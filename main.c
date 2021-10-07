8ceb11d9885d0cb5c32eedf9d32e236fb991e19a
Jairaj <67155126+jairaj09@users.noreply.github.com>
2021-10-07 12:59:37 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
 
 int main()
 {
-    char str[40], ch;
-    int i=0,len=0,j=0;
-    printf("Enter the string : ");
-    scanf("%s",&str);
-    len=strlen(str);
-    for(i=0;i<=len;i++)
+    int n , a=0 , b=0 ;
+    printf("Enter the number to check : ");
+    scanf("%d",&n);
+    b=n;
+    while(n!=0)
     {
-       if(str[i] != str[len-i-1])
-          {
-              j=1;
-              break;
-          }
+        a=a*10 + n%10;
+        n=n/10;
+    };
+    if(a==b)
+    {
+        printf("The number is a pelindrome.");
     }
-       if (j==0) {
-        printf("String is a palindrome");
+    else
+    {
+        printf("The number is not a pelindrome");
     }
-    else {
-        printf("String is not a palindrome");
-    }
-
     return 0;
 }
